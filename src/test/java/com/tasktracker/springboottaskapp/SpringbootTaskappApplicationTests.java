47f6b384d08eec4826826d6fef2838fef2e549b0
hgelle <hibaq.gelle1@gmail.com>
2022-10-28 00:34:22 -0500

    initial

---
+++ src/test/java/com/tasktracker/springboottaskapp/SpringbootTaskappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasktracker.springboottaskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTaskappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
